82f854e3c36e688f84938ed9690fe5c7ab6b9e8c
sajmikn <szymon@nikitin.io>
2022-07-22 15:50:01 +0200

    Alfa 1.0
    font size correct
    shop
    background
    ....

--- Shoper.js
+++ Shoper.js
@@ -18,6 +18,7 @@ const CoinShop = document.getElementById("CoinShop")
 const CoinCountShop = document.getElementById("CoinCountShop")
 const DiamondCountShop = document.getElementById("DiamondCountShop")
 const AddRemove = document.getElementById("addRemove")
+const cave = document.getElementById("cave")
 function ShopOpen(){
 	AllOne.style.display="none"
 	ShopElements.style.display=""
@@ -25,25 +26,38 @@ function ShopOpen(){
 function GetScrollTop(){
 	return Math.round(window.scrollY/window.innerHeight*100)
 }
-async function DiamondCoinAnimation(Num){
+function DiamondCoinAnimation(Num){
 let y = GetScrollTop()
 	CoinCountShop.innerText= "+" + Num
-	DiamondCountShop.innerText= "-" + Num/2.5
+	DiamondCountShop.innerText= "-" + Num/2
 	CoinCountShop.style.top = y + 50 + "vh"
 	CoinShop.style.top = y + 50 + "vh"
 	DiamondCountShop.style.top = y + 50 + "vh"
 	DiamondShop.style.top = y + 50 + "vh"
 	AddRemove.style.display=""
+	setTimeout(function (){
+		AddRemove.style.display="none"
+	}, 2000)
 }
 function CoinsAdd(Num){
 	DiamondCoinAnimation(Num)
-
+	let returnCoins = parseInt(localStorage.getItem('coins')) + Num
+	let returnDiamond = parseInt(localStorage.getItem('diamond')) - (Num/2)
+	localStorage.setItem('coins', returnCoins)
+	localStorage.setItem('diamond', returnDiamond)
 }
 
 Not.addEventListener("click", function (){
 	ShopElements.style.display="none"
 	AllOne.style.display=""
+	CloseShop()
 })
 CoinOne.addEventListener("click", function (){
-CoinsAdd(100)
+	if(localStorage.getItem('diamond')>=50){CoinsAdd(100)}
+})
+CoinTwo.addEventListener("click", function (){
+	if(localStorage.getItem('diamond')>=100){CoinsAdd(200)}
+})
+CoinTre.addEventListener("click", function (){
+	if(localStorage.getItem('diamond')>=250){CoinsAdd(500)}
 })
